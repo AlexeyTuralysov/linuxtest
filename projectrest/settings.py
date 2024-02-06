fd9ab77ca15c1e02e9bc997dce9ed114c201cf54
AlexeyMarkul <72796090+AlexeyMarkul@users.noreply.github.com>
2024-02-07 07:53:57 +0900

    collestatic

--- projectrest/settings.py
+++ projectrest/settings.py
@@ -122,7 +122,7 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 import os
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
