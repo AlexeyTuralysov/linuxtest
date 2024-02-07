b4eaf1a159a0b9783962471b787b7eb5bfea15f4
AlexeyMarkul <72796090+AlexeyMarkul@users.noreply.github.com>
2024-02-07 19:59:54 +0900

    static url

--- projectrest/settings.py
+++ projectrest/settings.py
@@ -123,7 +123,7 @@ REST_FRAMEWORK = {
 
 import os
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'linuxtest/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
